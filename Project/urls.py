0ec3e6834545901923536e98d3ffdda9360bcd11
MezoPeeta <wildlifemain1@gmail.com>
2019-07-18 16:42:51 +0200

    Finally

--- Project/urls.py
+++ Project/urls.py
@@ -23,5 +23,8 @@ urlpatterns = [
     path('admin/', include('admin_honeypot.urls', namespace='admin_honeypot')),
     path('me/', admin.site.urls),
     path('', include('blog.urls')),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
